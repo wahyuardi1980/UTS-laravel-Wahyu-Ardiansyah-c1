06256ed47215b71ac4e7b0507b1c3982d699ef28
Wahyu Ardiansyah <65841387+wahyuardi1980@users.noreply.github.com>
2023-11-17 13:50:39 +0700

    soal no 2 membuat isi tabel dengan array

---
+++ resources/views/product.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman | Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    {{-- navbar --}}
+    <nav class="navbar bg-body-tertiary fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">UTS Laravel</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">UTS Laravel</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ url('/product') }}">Produk</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg">
+                <h1 class="judul-produk">Halaman Produk</h1>
+            </div>
+            <div class="col-lg">
+                <div class="d-flex justify-content-end tombol-tambah">
+                    <a class="btn btn-primary" href="{{ url('/form') }}" role="button">Tambah Produk</a>
+                </div>
+            </div>
+        </div>
+
+        <table class="table table-bordered table-primary">
+            <thead>
+                <tr>
+                    <th>Kode Produk</th>
+                    <th>Nama Produk</th>
+                    <th>Jenis Produk</th>
+                    <th>Harga</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $nilai_awal = 0;
+                @endphp
+                @for ($i = 0; $i < $jumlah; $i++)
+                    <tr>
+                        <td>{{ $kode_produk[$i] }}</td>
+                        <td>{{ $nama_produk[$i] }}</td>
+                        <td>{{ $jenis_produk[$i] }}</td>
+                        <td>{{ $harga[$i] }}</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
